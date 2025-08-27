4d8583e73f253d32b753462808b3e496daaf4845
Mustafa Şükrü Kapusuz <m@mskapusuz.com>
2025-08-28 00:47:42 +0300

    init

---
+++ studypeak-certificate.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Studypeak Certificate
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     studypeak-certificate
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Studypeak_Certificate
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Studypeak_Certificate
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
